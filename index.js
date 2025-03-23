f7c6355f7e38c2134ec42b4aaccfd23183f7e9ad
Meenadharshini R S <rsmd7746@gmail.com>
2025-03-23 18:47:08 +0530

    App Default Port Change.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
-const mysql = require('mysql');
-const port = 3000
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.use(bodyparser.json());
