57c78053b23a3f0470a2a223d52ba2c5c8169642
Meenadharshini R S <rsmd7746@gmail.com>
2025-04-08 16:24:19 +0530

    Final Version

--- controller/hydroponicsContoller.js
+++ controller/hydroponicsContoller.js
@@ -5,5 +5,8 @@ const hydroponics = require('../services/hydroponicsService');
 router.get('/push/:name/:value',hydroponics.push);
 router.get('/', hydroponics.fetch);
 router.get('/refresh', hydroponics.refresh);
-
+router.get('/NutrientLevel', hydroponics.nurtrients);
+router.get('/GrowLED', hydroponics.grow_led);
+router.get('/OxygenMotor', hydroponics.o2_motor);
+router.get('/WaterPump', hydroponics.water_pump);
 module.exports = router;
--- services/hydroponicsService.js
+++ services/hydroponicsService.js
@@ -31,6 +31,8 @@ exports.fetch = (req, res) => {
         ORDER BY
             C.PARAMETER_ID
     `
+    // res.render('homepage', { Status: null });
+
     Db_Connection.Open(Connection => {
         Db_Connection.ExecuteQuery(Connection, qry, null, (_Cn, Results) => {
             ParamStatus = [];
@@ -38,8 +40,7 @@ exports.fetch = (req, res) => {
                 ParamStatus.push(result);
             });
             console.log(ParamStatus);
-            res.render('homepage', { Status: ParamStatus });
-            // res.json(ParamStatus);
+            res.render('homepage', { Title: "Current Status", Status: ParamStatus });
         });
     });
 };
@@ -74,6 +75,46 @@ exports.refresh = (req, res) => {
             console.log(ParamStatus);
             res.json(ParamStatus);
             // res.json(ParamStatus);
+        });
+    });
+};
+
+exports.nurtrients = (req, res) => {
+    qry = `SELECT TIMESTAMP, CONCAT(STATUS_VALUE, ' PPM') AS VALUE FROM PARAMETER_STATUS WHERE PARAMETER_ID = 4 AND STATUS_VALUE NOT IN ('ON', 'OFF') ORDER BY TIMESTAMP DESC`;
+    Db_Connection.Open(Connection => {
+        Db_Connection.ExecuteQuery(Connection, qry, null, (_Cn, Results) => {
+            NutrientLevels = Results.recordset;
+            res.render('TableGraph', { Title: "Nutrient Level", SubTitle: "Dissolved Nutrients over time", Data: JSON.stringify(NutrientLevels), Status: NutrientLevels });
+        });
+    });
+};
+
+exports.water_pump = (req, res) => {
+    qry = `SELECT TIMESTAMP, CONCAT(STATUS_VALUE, '') AS VALUE FROM PARAMETER_STATUS WHERE PARAMETER_ID = 1 ORDER BY TIMESTAMP DESC`;
+    Db_Connection.Open(Connection => {
+        Db_Connection.ExecuteQuery(Connection, qry, null, (_Cn, Results) => {
+            NutrientLevels = Results.recordset;
+            res.render('Table', { Title: "Water Pump", SubTitle: "Water Pump Function Status over time", Status: NutrientLevels });
+        });
+    });
+};
+
+exports.grow_led = (req, res) => {
+    qry = `SELECT TIMESTAMP, CONCAT(STATUS_VALUE, '') AS VALUE FROM PARAMETER_STATUS WHERE PARAMETER_ID = 3 ORDER BY TIMESTAMP DESC`;
+    Db_Connection.Open(Connection => {
+        Db_Connection.ExecuteQuery(Connection, qry, null, (_Cn, Results) => {
+            NutrientLevels = Results.recordset;
+            res.render('Table', { Title: "GROW LED", SubTitle: "Grow LED Function Status over time", Status: NutrientLevels });
+        });
+    });
+};
+
+exports.o2_motor = (req, res) => {
+    qry = `SELECT TIMESTAMP, CONCAT(STATUS_VALUE, '') AS VALUE FROM PARAMETER_STATUS WHERE PARAMETER_ID = 2 ORDER BY TIMESTAMP DESC`;
+    Db_Connection.Open(Connection => {
+        Db_Connection.ExecuteQuery(Connection, qry, null, (_Cn, Results) => {
+            NutrientLevels = Results.recordset;
+            res.render('Table', { Title: "Oxygen Motor", SubTitle: "Oxygen Motor Function Status over time", Status: NutrientLevels });
         });
     });
 };
