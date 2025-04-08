2430051a01411f5625dbb9e5ae1a64b42009b518
Meenadharshini R S <rsmd7746@gmail.com>
2025-04-08 16:47:00 +0530

    Arduino Code

--- src/android.ino
+++ src/android.ino
@@ -13,26 +13,21 @@
 #define FLT 2  // D4
 
 #define LED 14 // D5
+#define MAX_LED_DELAY 3600  // 3600 Secs => 60 Mins => 1 Hour.
+int LedDelay = MAX_LED_DELAY;
+bool LedStatus = false;
+
 #define O2M 12 // D6
+#define MAX_O2M_DELAY 300   // 300 Secs  =>  5 Mins.
+int O2MDelay = MAX_O2M_DELAY;
+bool O2MStatus = false;
 
-int ledTimerMax = 2 * 60; // 2 Mins
-int o2mTimerMax = 3 * 60; // 3 Mins
-int ledTimer = 0;
-int o2mTimer = 0;
-bool ledStatus = false;
-bool o2mStatus = false;
-
-const char* ssid = "srmp jio";  // Replace with your WiFi SSID
-const char* password = "7598150639";  // Replace with your WiFi Password
-const char* serverUrl = "https://hydroponics-md.azurewebsites.net/push/2/OFF";  // Azure API Base URL
-
-WiFiClientSecure client;  // Secure client for HTTPS
-HTTPClient http;
-
-unsigned long previousMillis = 0;
-const long interval = 40000; // 40 seconds
-
-bool status = false;
+const char* ssid = "ASSA";          // Replace with your WiFi SSID
+const char* password = "creative";  // Replace with your WiFi Password
+WiFiClientSecure client;            // Secure client for HTTPS
+HTTPClient http;                    // Http Request object
+int tds_observed = 0;               // Previously Measured TDS Reading
+int water_level_observed = 0;       //
 
 void setup() {
     Serial.begin(115200);
@@ -64,106 +59,81 @@ void setup() {
 
 void saveValues(String id, String value) {
   String url = "https://hydroponics-md.azurewebsites.net/push/" + id + "/" + value;
-  http.begin(client, url);
-  Serial.print("Connecting to: ");
   Serial.println(url);
+  http.begin(client, url);
   int response = http.GET();
-  Serial.print("HTTP Response Code: ");
-  Serial.println(response);
   http.end();
 }
 
 void loop() {
-  unsigned long start = millis();
+  Serial.println("Looping...");
   if (WiFi.status() == WL_CONNECTED) {
-    // 1. Check and Write Water Pump Status
-    int level;
-    level = digitalRead(FLT);
-    if (level == HIGH) {
-      Serial.println("Water Level: HIGH");
-      // Water Refilled, Switch Off Pump
-      digitalWrite(IN2, LOW);
-      saveValues("1", "OFF");
-    }
-    else {
-      Serial.println("Water Level: LOW");
-      // Low Water in System, Turn ON Pump
-      saveValues("1", "ON");
-      digitalWrite(IN2, HIGH);
-    }
-
-
-    // 2. O2 Motor
-    if (o2mTimer != 0) {
-      o2mTimer--;
-    }
-    else if(o2mTimer == 0) {
-      o2mTimer = o2mTimerMax;
-
-      if(o2mStatus == false){
-        //o2 Motor is ON... Time Out, Turn off Now
-        digitalWrite(O2M, LOW);
-        saveValues("2", "OFF");
-      }
-      else{
-        //o2 Motor is OFF... Time Out, Turn on Now...
-        digitalWrite(O2M, HIGH);
-        saveValues("2", "ON");
-      }
-
-    }
-
-    // 3. LED 
-    if (ledTimer != 0) {
-      ledTimer--;
-    }
-    else if (ledTimer == 0) {
-      ledTimer = ledTimerMax;
-
-      if(ledStatus == false){
-        //LED is ON... Time Out, Turn off Now...
-        digitalWrite(LED, LOW);
-        saveValues("3", "OFF");
-      }
-      else{
-        //LED is OFF... Time Out, Turn on Now...
-        digitalWrite(LED, HIGH);
-        saveValues("3", "ON");
-      }
-    }
-
-
-    // 4. Measure TDS and Log Values - Check Plant Nutrients
-    float SensorValue = analogRead(A0) - 7.0;
-    float ecValue = SensorValue * 3.3 / ADC_RESOLUTION;
-    int tds = ecValue * 1000;
-
-    Serial.print("Sensor Value: ");
-    Serial.print(SensorValue);
-    Serial.print("V;   EC Value: ");
-    Serial.print(ecValue);
-    Serial.print("V;   TDS Value: ");
-    Serial.print(tds);
-    Serial.println(" PPM");
-    saveValues("4", String(tds));
-    if (tds < 300) {
-      // Poor or Insufficient Nutirients, Drain Water in System and Refill
-      digitalWrite(IN4, HIGH);
-    }
+    HandleWaterLevel();   // 1. Check and Write Water Pump Status
+    HandleO2Motor();      // 2. O2 Motor
+    HandleGrowLED();      // 3. Grow LED 
+    HandleTDS();          // 4. TDS
+    Serial.println("Completed...");
   }
   else {
     Serial.println("Unable to Connect Wifi...");
   }
-  unsigned long end = millis();
-  Serial.print("Start: ");
-  Serial.print(start);
-  Serial.print("End: ");
-  Serial.print(end);
-  Serial.print("Time Taken: ");
-  Serial.println(end - start);
+}
 
-  if ((end - start) < 5000) {
-    delay(5000 - (end - start));
+void HandleWaterLevel() {
+  int level;
+  level = digitalRead(FLT);
+  if (water_level_observed != level) {
+    digitalWrite(IN2, level == HIGH ? LOW : HIGH);
+    saveValues("1", level == HIGH ? "OFF" : "ON");
+    water_level_observed = level;
   }
 }
 
+void HandleGrowLED() {
+  if (LedDelay == 1) {
+    //Reached Timeout;
+    digitalWrite(LED, LedStatus ? LOW : HIGH);
+    saveValues("3", LedStatus ? "ON" : "OFF");
+    LedStatus = !LedStatus;
+    LedDelay = MAX_LED_DELAY;
+  }
+  else {
+    LedDelay--;
+  }
+Serial.print("LED Delay: ");
+Serial.println(LedDelay);
+}
+
+void HandleO2Motor() {
+  if (O2MDelay == 1) {
+    //Reached Timeout;
+    digitalWrite(O2M, O2MStatus ? LOW : HIGH);
+    saveValues("2", O2MStatus ? "ON" : "OFF");
+    O2MStatus = !O2MStatus;
+    O2MDelay = MAX_O2M_DELAY;
+  }
+  else {
+    O2MDelay--;
+  }
+Serial.print("O2 Delay: ");
+Serial.println(O2MDelay);
+}
+
+void HandleTDS() {
+  float SensorValue = analogRead(A0) - 7.0;
+  float ecValue = SensorValue * 3.3 / ADC_RESOLUTION;
+  int tds_current = ecValue * 1000;
+
+  if (tds_observed != tds_current) {
+    saveValues("4", String(tds_current));
+    tds_observed = tds_current;
+  }
+
+  if (tds_current < 300) {
+    // Poor or Insufficient Nutirients, Drain Water in System and Refill
+    digitalWrite(IN4, HIGH);
+    // Turn ON Drain Pump for 2 Minutes...
+    delay(2000 * 60);
+    digitalWrite(IN4, LOW);
+  }
+}
